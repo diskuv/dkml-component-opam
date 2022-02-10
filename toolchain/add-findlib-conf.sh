8165abc03aa568f64f1477f84d23eda4a60712b6
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-02-09 19:43:48 -0800

    Use findlib.conf.d feature

--- toolchain/add-findlib-conf.sh
+++ toolchain/add-findlib-conf.sh
@@ -6,6 +6,11 @@
 # HOSTLIB_DIR is the lib/ directory for the host ABI that contains
 # `findlib.conf` among others.
 #
+# The generated findlib configuration file will be
+# <HOSTLIB_DIR>/findlib.conf.d/<toolchain>.conf for compatibility
+# with Dune's `dune -x` option. The generation is mostly idempotent
+# since, if the file already exsts, entries are only added to FINDLIB_CONF.
+#
 # MLCROSS_DIR is the base directory for zero or more target DKML ABIs (ex.
 # android_arm32v7a) that contains bin/ and lib/ subfolders (ex.
 # android_arm32v7a/bin). It can be an environment variable, but a default value
@@ -41,8 +46,6 @@ if [ -z "${MLCROSS_DIR:-}" ]; then
 fi
 shift
 
-FINDLIB_CONF=$HOSTLIB_DIR/findlib.conf
-
 # [add_if_missing FILE SEARCH TEXT_IF_MISSING]
 add_if_missing() {
   add_if_missing_FILE=$1
@@ -72,6 +75,7 @@ set +u # Fix bash bug with empty arrays
 for _crossdir in "${CROSSES[@]}"; do
     # ex. android_arm32v7a
     dkmlabi=$(basename "$_crossdir")
+    findlib_conf="$HOSTLIB_DIR/findlib.conf.d/$dkmlabi.conf"
 
     # Ex. path(android_arm32v7a)="C:\\source\\windows_x86_64\\lib"
     # Any backslashes need to be escaped since it is an OCaml string
@@ -96,28 +100,28 @@ for _crossdir in "${CROSSES[@]}"; do
     bin_buildhost=$(_escape_arg_as_ocaml_string "$bin_buildhost")
     lib_buildhost=$(_escape_arg_as_ocaml_string "$lib_buildhost")
 
-    add_if_missing "$FINDLIB_CONF" "^path($dkmlabi)"            "path($dkmlabi) = \"$lib_buildhost${_findsep}$libhost_buildhost\""
-    add_if_missing "$FINDLIB_CONF" "^destdir($dkmlabi)"         "destdir($dkmlabi) = \"$lib_buildhost\""
-    add_if_missing "$FINDLIB_CONF" "^stdlib($dkmlabi)"          "stdlib($dkmlabi) = \"$lib_buildhost${_dirsep}ocaml\""
+    add_if_missing "$findlib_conf" "^path($dkmlabi)"            "path($dkmlabi) = \"$lib_buildhost${_findsep}$libhost_buildhost\""
+    add_if_missing "$findlib_conf" "^destdir($dkmlabi)"         "destdir($dkmlabi) = \"$lib_buildhost\""
+    add_if_missing "$findlib_conf" "^stdlib($dkmlabi)"          "stdlib($dkmlabi) = \"$lib_buildhost${_dirsep}ocaml\""
     if [ -e "$_crossdir/bin/flexlink.exe" ]; then
-        add_if_missing "$FINDLIB_CONF" "^flexlink($dkmlabi)"    "flexlink($dkmlabi) = \"$bin_buildhost${_dirsep}flexlink.exe\""
+        add_if_missing "$findlib_conf" "^flexlink($dkmlabi)"    "flexlink($dkmlabi) = \"$bin_buildhost${_dirsep}flexlink.exe\""
     fi
-    add_if_missing "$FINDLIB_CONF" "^ocaml($dkmlabi)"           "ocaml($dkmlabi) = \"$bin_buildhost${_dirsep}ocaml${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlc($dkmlabi)"          "ocamlc($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlc.opt${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlcmt($dkmlabi)"        "ocamlcmt($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlcmt${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlcp($dkmlabi)"         "ocamlcp($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlcp.byte${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamldebug($dkmlabi)"      "ocamldebug($dkmlabi) = \"$bin_buildhost${_dirsep}ocamldebug${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamldep($dkmlabi)"        "ocamldep($dkmlabi) = \"$bin_buildhost${_dirsep}ocamldep.byte${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamllex($dkmlabi)"        "ocamllex($dkmlabi) = \"$bin_buildhost${_dirsep}ocamllex.opt${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlmklib($dkmlabi)"      "ocamlmklib($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlmklib.byte${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlmktop($dkmlabi)"      "ocamlmktop($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlmktop.byte${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlobjinfo($dkmlabi)"    "ocamlobjinfo($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlobjinfo.byte${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlopt($dkmlabi)"        "ocamlopt($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlopt.opt${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamloptp($dkmlabi)"       "ocamloptp($dkmlabi) = \"$bin_buildhost${_dirsep}ocamloptp.byte${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlprof($dkmlabi)"       "ocamlprof($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlprof.byte${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlrun($dkmlabi)"        "ocamlrun($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlrun${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlrund($dkmlabi)"       "ocamlrund($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlrund${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlruni($dkmlabi)"       "ocamlruni($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlruni${_exe}\""
-    add_if_missing "$FINDLIB_CONF" "^ocamlyacc($dkmlabi)"       "ocamlyacc($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlyacc${_exe}\""
+    add_if_missing "$findlib_conf" "^ocaml($dkmlabi)"           "ocaml($dkmlabi) = \"$bin_buildhost${_dirsep}ocaml${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlc($dkmlabi)"          "ocamlc($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlc.opt${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlcmt($dkmlabi)"        "ocamlcmt($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlcmt${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlcp($dkmlabi)"         "ocamlcp($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlcp.byte${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamldebug($dkmlabi)"      "ocamldebug($dkmlabi) = \"$bin_buildhost${_dirsep}ocamldebug${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamldep($dkmlabi)"        "ocamldep($dkmlabi) = \"$bin_buildhost${_dirsep}ocamldep.byte${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamllex($dkmlabi)"        "ocamllex($dkmlabi) = \"$bin_buildhost${_dirsep}ocamllex.opt${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlmklib($dkmlabi)"      "ocamlmklib($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlmklib.byte${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlmktop($dkmlabi)"      "ocamlmktop($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlmktop.byte${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlobjinfo($dkmlabi)"    "ocamlobjinfo($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlobjinfo.byte${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlopt($dkmlabi)"        "ocamlopt($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlopt.opt${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamloptp($dkmlabi)"       "ocamloptp($dkmlabi) = \"$bin_buildhost${_dirsep}ocamloptp.byte${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlprof($dkmlabi)"       "ocamlprof($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlprof.byte${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlrun($dkmlabi)"        "ocamlrun($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlrun${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlrund($dkmlabi)"       "ocamlrund($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlrund${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlruni($dkmlabi)"       "ocamlruni($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlruni${_exe}\""
+    add_if_missing "$findlib_conf" "^ocamlyacc($dkmlabi)"       "ocamlyacc($dkmlabi) = \"$bin_buildhost${_dirsep}ocamlyacc${_exe}\""
 done
 set -u
