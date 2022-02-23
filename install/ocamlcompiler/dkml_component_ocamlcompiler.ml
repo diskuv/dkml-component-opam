b634653c7991938ca71668ac7dcaae7a91227401
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-02-22 17:50:02 -0800

    Define install_user_subcommand

--- install/ocamlcompiler/dkml_component_ocamlcompiler.ml
+++ install/ocamlcompiler/dkml_component_ocamlcompiler.ml
@@ -2,6 +2,10 @@ open Dkml_install_register
 
 let run = function Ok () -> () | Error str -> failwith str
 
+let execute_install ctx =
+  Format.printf "We can run bytecode using: %s@\n"
+    (ctx.Dkml_install_api.Context.path_eval "%{ocamlrun:share}/bin/ocamlrun.exe")
+
 let () =
   let reg = Component_registry.get () in
   run
@@ -12,4 +16,10 @@ let () =
          let component_name = "ocamlcompiler"
 
          let depends_on = [ "ocamlrun" ]
+
+         let install_user_subcommand ~component_name ~subcommand_name ~ctx_t =
+           let doc = "Install the OCaml compiler" in
+           Result.ok
+           @@ Cmdliner.Term.
+                (const execute_install $ ctx_t, info subcommand_name ~doc)
        end)
