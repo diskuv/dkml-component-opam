c8df826e151751af092de54e0e3b81035fdcf457
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-03-02 06:59:55 -0800

    Use error handling from API

--- install/ocamlcompiler/dkml_component_ocamlcompiler.ml
+++ install/ocamlcompiler/dkml_component_ocamlcompiler.ml
@@ -1,66 +1,42 @@
+open Dkml_install_api
 open Dkml_install_register
 open Bos
-
-let run = function Ok () -> () | Error str -> failwith str
 
 let execute_install ctx =
   Logs.info (fun m ->
       m "The name of the currently installing component is: %s"
-        (ctx.Dkml_install_api.Context.eval "%{name}%"));
+        (ctx.Context.eval "%{name}%"));
   Logs.info (fun m ->
       m "The available components are: %s"
-        (ctx.Dkml_install_api.Context.eval "%{components:all}%"));
+        (ctx.Context.eval "%{components:all}%"));
   Logs.info (fun m ->
       m "The install location is: %a" Fpath.pp
-        (ctx.Dkml_install_api.Context.path_eval "%{prefix}%"));
+        (ctx.Context.path_eval "%{prefix}%"));
   Logs.info (fun m ->
       m "We can place temporary files in: %a" Fpath.pp
-        (ctx.Dkml_install_api.Context.path_eval "%{tmp}%"));
-  let bytecode =
-    ctx.Dkml_install_api.Context.path_eval "%{_:share}%/generic/install.bc"
-  in
+        (ctx.Context.path_eval "%{tmp}%"));
+  let bytecode = ctx.Context.path_eval "%{_:share}%/generic/install.bc" in
   Logs.info (fun m -> m "Our bytecode executable is at: %a" Fpath.pp bytecode);
-  let ocamlrun =
-    ctx.Dkml_install_api.Context.path_eval "%{ocamlrun:share}%/bin/ocamlrun.exe"
-  in
+  let ocamlrun = ctx.Context.path_eval "%{ocamlrun:share}%/bin/ocamlrun.exe" in
   Logs.info (fun m ->
       m "We will run bytecode using: %a" Fpath.pp
-        (ctx.Dkml_install_api.Context.path_eval
-           "%{ocamlrun:share}%/bin/ocamlrun.exe"));
-  match
-    OS.Cmd.run_status
-      Cmd.(v (Fpath.to_string ocamlrun) % Fpath.to_string bytecode)
-  with
-  | Ok (`Exited 0) ->
-      Logs.info (fun m ->
-          m "The bytecode executable %a ran successfully" Fpath.pp bytecode)
-  | Ok (`Exited c) ->
-      Logs.err (fun m ->
-          m "The bytecode executable %a exited with status %d" Fpath.pp bytecode
-            c)
-  | Ok (`Signaled c) ->
-      Logs.err (fun m ->
-          m "The bytecode executable %a terminated from a signal %d" Fpath.pp
-            bytecode c)
-  | Error rmsg ->
-      Logs.err (fun m ->
-          m "The bytecode executable %a could not be run due to: %a" Fpath.pp
-            bytecode Rresult.R.pp_msg rmsg)
+        (ctx.Context.path_eval "%{ocamlrun:share}%/bin/ocamlrun.exe"));
+  log_spawn_and_raise
+    Cmd.(v (Fpath.to_string ocamlrun) % Fpath.to_string bytecode)
 
 let () =
   let reg = Component_registry.get () in
-  run
-  @@ Component_registry.add_component reg
-       (module struct
-         include Dkml_install_api.Default_component_config
+  Component_registry.add_component reg
+    (module struct
+      include Default_component_config
 
-         let component_name = "ocamlcompiler"
+      let component_name = "ocamlcompiler"
 
-         let depends_on = [ "ocamlrun"; "unixutils" ]
+      let depends_on = [ "ocamlrun"; "unixutils" ]
 
-         let install_user_subcommand ~component_name ~subcommand_name ~ctx_t =
-           let doc = "Install the OCaml compiler" in
-           Result.ok
-           @@ Cmdliner.Term.
-                (const execute_install $ ctx_t, info subcommand_name ~doc)
-       end)
+      let install_user_subcommand ~component_name ~subcommand_name ~ctx_t =
+        let doc = "Install the OCaml compiler" in
+        Result.ok
+        @@ Cmdliner.Term.
+             (const execute_install $ ctx_t, info subcommand_name ~doc)
+    end)
--- install/ocamlrun/dkml_component_ocamlrun.ml
+++ install/ocamlrun/dkml_component_ocamlrun.ml
@@ -1,15 +1,12 @@
 open Dkml_install_register
 
-let run = function Ok () -> () | Error str -> failwith str
-
 let () =
   let reg = Component_registry.get () in
-  run
-  @@ Component_registry.add_component reg
-       (module struct
-         include Dkml_install_api.Default_component_config
+  Component_registry.add_component reg
+    (module struct
+      include Dkml_install_api.Default_component_config
 
-         let component_name = "ocamlrun"
+      let component_name = "ocamlrun"
 
-         let depends_on = []
-       end)
+      let depends_on = []
+    end)
