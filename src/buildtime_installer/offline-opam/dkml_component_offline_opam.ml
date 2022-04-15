7e83aaf7a2c53ef922ec0bc6ed427bdf02ae54ad
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-04-15 09:42:00 -0700

    Add offline-opam component

---
+++ src/buildtime_installer/offline-opam/dkml_component_offline_opam.ml
@@ -0,0 +1,12 @@
+open Dkml_install_api
+open Dkml_install_register
+
+let register () =
+  let reg = Component_registry.get () in
+  Component_registry.add_component reg
+    (module struct
+      include Default_component_config
+
+      let component_name = "offline-opam"
+
+    end)
