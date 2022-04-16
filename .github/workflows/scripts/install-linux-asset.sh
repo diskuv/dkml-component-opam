a0deeae6ec44d982cdc12c9225e6eb15a881b59a
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-04-16 00:50:19 -0700

    Dump config.log

--- .github/workflows/scripts/install-linux-asset.sh
+++ .github/workflows/scripts/install-linux-asset.sh
@@ -37,7 +37,17 @@ sh -x .github/workflows/scripts/pin-unix-asset.sh
 OPAMROOT=$(opam var root)
 if ! opam install ./"$COMPONENT".opam --with-test  --yes; then
     printf "\n\n========= [START OF TROUBLESHOOTING] ===========\n\n" >&2
-    env >&2
+
+    # find "$OPAMROOT" -name config.log >&2
+    tree "$OPAMROOT/ci/share/dkml-component-offline-opam/static-files/src/opam" || find "$OPAMROOT/ci/share/dkml-component-offline-opam/static-files/src/opam"
+
+    for i in config.log bootstrap/ocaml-4.12.0/config.log; do
+        dump_on_error_BLOG="$OPAMROOT"/ci/share/dkml-component-offline-opam/static-files/src/opam/"$i"
+        if [ -e "$dump_on_error_BLOG" ]; then
+            printf "\n\n========= [TROUBLESHOOTING] %s ===========\n\n" "$dump_on_error_BLOG" >&2
+            cat "$dump_on_error_BLOG" >&2
+        fi
+    done
 
     find "$OPAMROOT"/log -mindepth 1 -maxdepth 1 -name "*.env" ! -name "log-*.env" ! -name "ocaml-variants-*.env" | head -n1 | while read -r dump_on_error_LOG; do
         dump_on_error_BLOG=$(basename "$dump_on_error_LOG")
