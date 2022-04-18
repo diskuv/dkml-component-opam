f7734dfba0823c0395e5a2d29d72c3edc962f333
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-04-18 02:05:05 -0700

    Switch fdopen bootstrap to setup-dkml(17a)
    Remove blank line from switch-config
    Use array syntax in switch-config. Close parens
    Use opam config list instead of opam option.
    Fix typo in dist_name.
    Upload Opam.Runtime.amd64.
    Remove duplicate setenv.
    Use opam env after converting to DOS format.
    opam install --debug-level=4
    Use tar from MSYS2.
    Make logic for use-fdopen-opam=true and false (set to =true for now).
    Use mixed paths or relative paths with MSYS2 tar.
    Install uucd to fix test dependencies.
    Debug opam.out files; remove --debug-level=4; exclude Opam from Dune
    Use mixed path for tar bundling
    (a) use-fdopen-opam=true
    (b) use-fdopen-opam=false

--- .github/workflows/scripts/pin-unix-asset.sh
+++ .github/workflows/scripts/pin-unix-asset.sh
@@ -8,3 +8,12 @@ opam pin dkml-package-console                 'https://github.com/diskuv/dkml-in
 opam pin dkml-component-staging-curl          'https://github.com/diskuv/dkml-component-curl.git#main' --no-action --yes
 opam pin dkml-component-staging-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
 opam pin dkml-component-network-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
+
+# GitHub stalls with:
+#   <><> Error report <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+#   +- The following actions failed
+#   | - fetch uucp 14.0.0
+#   +-
+# Example: https://github.com/diskuv/dkml-component-ocamlcompiler/runs/6060230260?check_suite_focus=true
+# So pin it instead of download from Opam cache
+opam pin uucp                                 'git+https://erratique.ch/repos/uucp.git#v14.0.0' --no-action --yes
