1a3ffa6aaf7cae9f43b97c81db2773ba92d445bb
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-04-20 12:56:50 -0700

    Build ABIs using opamrun

--- .github/workflows/scripts/pin-unix-asset.sh
+++ .github/workflows/scripts/pin-unix-asset.sh
@@ -2,12 +2,12 @@
 set -eufx
 
 # until published in Opam repository
-opam pin dkml-install                         'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
-opam pin dkml-install-runner                  'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
-opam pin dkml-package-console                 'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
-opam pin dkml-component-staging-curl          'https://github.com/diskuv/dkml-component-curl.git#main' --no-action --yes
-opam pin dkml-component-staging-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
-opam pin dkml-component-network-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
+opamrun pin dkml-install                         'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
+opamrun pin dkml-install-runner                  'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
+opamrun pin dkml-package-console                 'https://github.com/diskuv/dkml-install-api.git#main' --no-action --yes
+opamrun pin dkml-component-staging-curl          'https://github.com/diskuv/dkml-component-curl.git#main' --no-action --yes
+opamrun pin dkml-component-staging-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
+opamrun pin dkml-component-network-unixutils     'https://github.com/diskuv/dkml-component-unixutils.git#main' --no-action --yes
 
 # GitHub stalls with:
 #   <><> Error report <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
@@ -16,4 +16,4 @@ opam pin dkml-component-network-unixutils     'https://github.com/diskuv/dkml-co
 #   +-
 # Example: https://github.com/diskuv/dkml-component-ocamlcompiler/runs/6060230260?check_suite_focus=true
 # So pin it instead of download from Opam cache
-opam pin uucp                                 'git+https://erratique.ch/repos/uucp.git#v14.0.0' --no-action --yes
+opamrun pin uucp                                 'git+https://erratique.ch/repos/uucp.git#v14.0.0' --no-action --yes
