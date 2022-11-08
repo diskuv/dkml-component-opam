c0b7e9f32dea2502a8042aae9bb4661070bcbf0d
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-11-08 08:25:34 -0800

    Use cmdliner for arg processing
    * cmdliner so can use Dkml_install_api.Log_config for
      --verbosity etc. arguments

--- src/installtime_enduser/opam_install.ml
+++ src/installtime_enduser/opam_install.ml
@@ -1,34 +1,44 @@
+open Cmdliner
+
 type t = { source_dir : Fpath.t; target_dir : Fpath.t }
 
 let create ~source_dir ~target_dir = { source_dir; target_dir }
 
-let install { source_dir; target_dir } =
+let install (_ : Dkml_install_api.Log_config.t) source_dir target_dir =
   match Diskuvbox.copy_dir ~src:source_dir ~dst:target_dir () with
   | Ok () -> ()
   | Error msg -> failwith msg
 
-let () =
-  (* Arg parsing *)
-  let anon_fun (_ : string) =
-    failwith "No commandline arguments supported for ocamlrun opam_install.ml"
+let source_dir_t =
+  let x =
+    Arg.(
+      required & opt (some dir) None & info ~doc:"Source path" [ "source-dir" ])
   in
-  let source_dir = ref "" in
-  let target_dir = ref "" in
-  Arg.(
-    parse
-      [
-        ("--source-dir", Set_string source_dir, "Source path");
-        ("--target-dir", Set_string target_dir, "Destination path");
-      ]
-      anon_fun "Install the desktop binaries in --source-dir into --target-dir");
-  if !source_dir = "" then (
-    prerr_endline "FATAL: The --source-dir DIR option is required.";
-    exit 1);
-  if !target_dir = "" then (
-    prerr_endline "FATAL: The --target-dir DIR option is required.";
-    exit 1);
+  Term.(const Fpath.v $ x)
 
-  let installer =
-    create ~source_dir:(Fpath.v !source_dir) ~target_dir:(Fpath.v !target_dir)
+let target_dir_t =
+  let x =
+    Arg.(
+      required
+      & opt (some string) None
+      & info ~doc:"Target path" [ "target-dir" ])
   in
-  install installer
+  Term.(const Fpath.v $ x)
+
+let setup_log style_renderer level =
+  Fmt_tty.setup_std_outputs ?style_renderer ();
+  Logs.set_level level;
+  Logs.set_reporter (Logs_fmt.reporter ());
+  Dkml_install_api.Log_config.create ?log_config_style_renderer:style_renderer
+    ?log_config_level:level ()
+
+let setup_log_t =
+  Term.(const setup_log $ Fmt_cli.style_renderer () $ Logs_cli.level ())
+
+let () =
+  let t =
+    Term.
+      ( const install $ setup_log_t $ source_dir_t $ target_dir_t,
+        info "opam-install.bc" ~doc:"Install opam" )
+  in
+  Term.(exit @@ eval t)
